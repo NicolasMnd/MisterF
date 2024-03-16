478a049f342dc5b034e12ee3d3015bc602d4b463
Nicolas <30669856+GiveWife@users.noreply.github.com>
2024-03-17 00:48:40 +0100

    Initial commit

---
+++ src/com/misterf/core/MisterF.java
@@ -0,0 +1,15 @@
+package com.misterf.core;
+
+import java.io.File;
+
+public class MisterF {
+
+    private final File file;
+
+    public MisterF(String path) {
+        this.file = new File(path);
+    }
+
+    
+
+}
